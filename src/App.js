cb754870527aab78737a0853dcfbc90d6e9c2b6f
devpablokim <tanhyu.kim@gmail.com>
2022-10-19 18:36:02 +0900

    1.0.2 Add hover & active functions

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 
-const Father = styled.div`
-  display: flex; 
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw; 
+  justify-content: center;
+  align-items: center;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px; 
-  height: 100px;
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 
-const BoxTwo = styled.div`
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
-  width: 100px; 
-  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation:${ rotateAnimation } 1s linear infinite;
+  ${Emoji} {
+    &:hover {
+      font-size: 90px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
-const Text = styled.span`
-  color: white;
-`;
 
 function App() {
   return (
-    <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji>😀</Emoji>
+      </Box>
+    </Wrapper>
   );
 }
 
